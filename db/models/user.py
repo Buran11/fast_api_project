4a0d4715d9cfe349067047bfc3ed09089dbbb3e5
Buran11 <ionovv1985@gmail.com>
2025-03-04 11:27:05 +0700

    add user, blog tables

---
+++ db/models/user.py
@@ -0,0 +1,17 @@
+from db.base_class import Base
+from sqlalchemy import (
+    Boolean, Column, Integer, String
+)
+from sqlalchemy.orm import relationship
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, nullable=False, unique=True, index=True)
+    password = Column(String, nullable=False)
+    is_superuser = Column(Boolean, default=False)
+    is_active = Column(Boolean, default=True)
+
+    blog = relationship("Blog", back_populates="author")
