0e1ff6860c6156d804946e2f564e072d4622c58f
Buran11 <ionovv1985@gmail.com>
2025-03-04 15:19:26 +0700

    add user, blog tables

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ from core.config import settings
 from db.session import engine
 from db.base import Base
 
+from apis.base import api_router
+
+
+def include_router(app):
+    app.include_router(api_router)
+
 
 def create_tables():
     Base.metadata.create_all(bind=engine)
@@ -12,6 +18,7 @@ def start_application():
     app = FastAPI(title=settings.PROJECT_NAME,
                   version=settings.PROJECT_VERSION)
     create_tables()
+    include_router(app)
     return app
 
 
--- schemas/blog.py
+++ schemas/blog.py
@@ -1,6 +1,1 @@
-from pydantic import BaseModel, Field, EmailStr
 
-
-class UserCreate(BaseModel):
-    email: EmailStr
-    password: str = Field(..., min_length=4)
